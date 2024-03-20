2586f4751d7516c745337740657f734dafedd268
thanos-dft <thanos.dft@gmail.com>
2024-03-20 19:42:45 +0200

    Python Code
    Commit#1 Pycode

---
+++ #BMI Calculator.py
@@ -0,0 +1,15 @@
+#BMI Calculator
+
+height = float(input())  # in meters e.g., 1.55
+weight = int(input())  # in kilograms e.g., 72
+bmi = weight / (height * height)
+if bmi < 18.5:
+  print(f"Your BMI is {bmi}, you are underweight.")
+elif bmi < 25:
+  print(f"Your BMI is {bmi}, you have a normal weight.")
+elif bmi < 30:
+  print(f"Your BMI is {bmi}, you are slightly overweight.")
+elif bmi < 35:
+  print(f"Your BMI is {bmi}, you are obese.")
+
+  #Dft
